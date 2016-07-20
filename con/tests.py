97feb5864b800f8e963e5c18410613c920699391
daniel <albarralnunez@github.com>
2016-07-20 08:56:37 +0200

    add simple test for eval

---
+++ con/tests.py
@@ -0,0 +1,30 @@
+import unittest
+from op_evalv2 import EvalExp
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test(self):
+        res = EvalExp('1 + 2 * 3 - 4').eval()
+        self.assertEqual(
+            res,
+            3
+        )
+        res = EvalExp('-1 + 2 * 3 - 4').eval()
+        self.assertEqual(
+            res,
+            1
+        )
+        res = EvalExp('1 + 2 * (3 + 3) / 2').eval()
+        self.assertEqual(
+            res,
+            7
+        )
+        res = EvalExp('(1 + 2 * (3 + 3) / 2)/2').eval()
+        self.assertEqual(
+            res,
+            3.5
+        )
+
+if __name__ == '__main__':
+    unittest.main()
